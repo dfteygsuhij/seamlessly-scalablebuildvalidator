5f1496f639b7b3ef45f0cacfb3f4a7c48c5f7320
dfteygsuhij <hsjcafizdhanani@hotmail.com>
2024-05-06 04:18:16 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const colors = ["red", "green", "blue"];
-const randomColor = colors[Math.floor(Math.random() * colors.length)];
-console.log(randomColor);
+function levelOrderBottom(root) {
+  if (!root) return [];
+  const result = [];
+  const queue = [root];
+  while (queue.length) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+      currentLevel.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.unshift(currentLevel);
+  }
+  return result;
+}
