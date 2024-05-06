9383a234c8f772f1a33e8510d2f19b36b3e24303
dfteygsuhij <hsjcafizdhanani@hotmail.com>
2024-05-06 13:31:02 +0700

    hello world

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-function levelOrderBottom(root) {
-  if (!root) return [];
-  const result = [];
-  const queue = [root];
-  while (queue.length) {
-    const levelSize = queue.length;
-    const currentLevel = [];
-    for (let i = 0; i < levelSize; i++) {
-      const node = queue.shift();
-      currentLevel.push(node.val);
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
-    }
-    result.unshift(currentLevel);
-  }
-  return result;
+function isPalindrome(str) {
+  return str === str.split("").reverse().join("");
 }
